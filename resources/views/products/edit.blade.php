346ba5578aed281df3d1c11f9a256a34ce8f261a
unknown <ducminhdmdm@gmail.com>
2022-02-25 21:40:19 +0700

    first commit

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,85 @@
+@extends('products.layout')
+@section('content')
+
+
+    <div class = "row">
+    <div class ="col-lg-12 margin-tb">
+        <div class ="pull-left">
+            <h2>Edit Product</h2>
+             
+        </div>
+        <div class ="pull-right">
+            <a class = "btn btn-primary" href="{{ route('products.index') }}">Back</a>
+
+        </div>
+    </div>
+    </div>
+
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Lỗi</strong>
+    <ul>
+        @foreach ($errors->all() as $error )
+            <li>{{ $error }}</li>
+            
+        @endforeach
+    </ul>
+
+</div>
+@endif
+
+<form action="{{ route('products.update',$product->id) }}" method="POST" >
+    @csrf
+    @method('PUT')
+
+    <div class="row">
+        <div class = "col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" value="{{ $product->name }}" class="form-control" plaecholder="Name">
+
+            </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class ="form-group">
+                <strong>Detail:</strong> 
+                <textarea class="form-control" name="detail" style="height:120px" placeholder="Detail" >
+                    {{ $product->detail }}
+                </textarea>
+                </div>    
+        </div>
+        <div class ="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">
+                Nhập
+            </button>
+        </div>
+
+
+    </div>
+
+</form>
+
+{{-- </div>
+<div class="row">
+    <div class ="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {{ $product->name }}
+
+        </div>
+
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class ="form-group">
+            <strong>Detail:</strong>
+            {{ $product->detail }} 
+            </div> 
+
+    </div>
+
+</div> --}}
+
+
+@endsection
